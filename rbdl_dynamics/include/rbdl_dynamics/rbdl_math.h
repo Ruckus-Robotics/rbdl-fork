dc756bac60a77d718e167d932bd74ecc4204740c
Jordan <jlack1987@gmail.com>
2016-05-23 16:42:23 -0500

    Fixed a few includes.

--- rbdl_dynamics/include/rbdl_dynamics/rbdl_math.h
+++ rbdl_dynamics/include/rbdl_dynamics/rbdl_math.h
@@ -19,14 +19,14 @@
 #include "rbdl_dynamics/SimpleMath/SimpleMathCommaInitializer.h"
 #include <vector>
 
-typedef SimpleMath::Fixed::Matrix<double, 3,1> Vector3_t;
-typedef SimpleMath::Fixed::Matrix<double, 3,3> Matrix3_t;
-typedef SimpleMath::Fixed::Matrix<double, 4,1> Vector4_t;
+typedef SimpleMath::Fixed::Matrix<double, 3, 1> Vector3_t;
+typedef SimpleMath::Fixed::Matrix<double, 3, 3> Matrix3_t;
+typedef SimpleMath::Fixed::Matrix<double, 4, 1> Vector4_t;
 
-typedef SimpleMath::Fixed::Matrix<double, 6,1> SpatialVector_t;
-typedef SimpleMath::Fixed::Matrix<double, 6,6> SpatialMatrix_t;
+typedef SimpleMath::Fixed::Matrix<double, 6, 1> SpatialVector_t;
+typedef SimpleMath::Fixed::Matrix<double, 6, 6> SpatialMatrix_t;
 
-typedef SimpleMath::Fixed::Matrix<double, 6,3> Matrix63_t;
+typedef SimpleMath::Fixed::Matrix<double, 6, 3> Matrix63_t;
 
 typedef SimpleMath::Dynamic::Matrix<double> MatrixN_t;
 typedef SimpleMath::Dynamic::Matrix<double> VectorN_t;
@@ -44,34 +44,36 @@ typedef Eigen::VectorXd VectorN_t;
 typedef Eigen::MatrixXd MatrixN_t;
 #endif
 
-namespace RigidBodyDynamics {
+namespace RigidBodyDynamics
+{
 
-/** \brief Math types such as vectors and matrices and utility functions. */
-namespace Math {
-typedef Vector3_t Vector3d;
-typedef Vector4_t Vector4d;
-typedef Matrix3_t Matrix3d;
-typedef SpatialVector_t SpatialVector;
-typedef SpatialMatrix_t SpatialMatrix;
-typedef Matrix63_t Matrix63;
-typedef VectorN_t VectorNd;
-typedef MatrixN_t MatrixNd;
-} /* Math */
+    /** \brief Math types such as vectors and matrices and utility functions. */
+    namespace Math
+    {
+        typedef Vector3_t Vector3d;
+        typedef Vector4_t Vector4d;
+        typedef Matrix3_t Matrix3d;
+        typedef SpatialVector_t SpatialVector;
+        typedef SpatialMatrix_t SpatialMatrix;
+        typedef Matrix63_t Matrix63;
+        typedef VectorN_t VectorNd;
+        typedef MatrixN_t MatrixNd;
+    } /* Math */
 
 } /* RigidBodyDynamics */
 
-#include "rbdl/Quaternion.h"
-#include "rbdl/SpatialAlgebraOperators.h"
+#include "rbdl_dynamics/Quaternion.h"
+#include "rbdl_dynamics/SpatialAlgebraOperators.h"
 
 // If we use Eigen3 we have to create specializations of the STL
 // std::vector such that the alignment is done properly.
 #ifndef RBDL_USE_SIMPLE_MATH
-  EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(RigidBodyDynamics::Math::SpatialVector)
-  EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(RigidBodyDynamics::Math::SpatialMatrix)
-  EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(RigidBodyDynamics::Math::Matrix63)
-  EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(RigidBodyDynamics::Math::SpatialTransform)
-  EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(RigidBodyDynamics::Math::SpatialRigidBodyInertia)
+EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(RigidBodyDynamics::Math::SpatialVector)
+EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(RigidBodyDynamics::Math::SpatialMatrix)
+EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(RigidBodyDynamics::Math::Matrix63)
+EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(RigidBodyDynamics::Math::SpatialTransform)
+EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(RigidBodyDynamics::Math::SpatialRigidBodyInertia)
 #endif
 
-  /* RBDL_MATH_H_H */
+/* RBDL_MATH_H_H */
 #endif
